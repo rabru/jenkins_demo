755d8fc795cff352693fbf8842e9d221ef66da54
plcharbonneau <charbonneaupl@gmail.com>
2017-02-10 16:27:20 -0500

    commit

--- main.py
+++ main.py
@@ -5,4 +5,4 @@ bigipaddr = "10.1.1.4"
 
 for f in glob.glob('./*.json.deploy'):
     print f
-    os.system("./scripts/deploy_iapp_bigip.py " + bigipaddr + " " + f)
+    os.system("./scripts/deploy_iapp_bigip.py -r " + bigipaddr + " " + f)
