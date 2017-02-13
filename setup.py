c6b423ee555eff200d2156104aff07a2e24c76e5
plcharbonneau <charbonneaupl@gmail.com>
2017-02-13 14:14:28 -0500

    commit

---
+++ setup.py
@@ -0,0 +1,18 @@
+from setuptools import setup
+
+setup(name='jenkins_demo',
+      version='0.1',
+      description='',
+      url='',
+      author='Pier-Luc Charbonneau',
+      author_email='Pier-Luc Charbonneau',
+      license='Apache License, Version 2.0',
+      packages=['jenkins_demo'],
+      install_requires=[
+          'json',
+          'argparse',
+          'sys',
+          'pprint',
+          'time',
+      ],
+      zip_safe=False)
