f679cc17c28ce47d15886d958bf35902df614365
plcharbonneau <charbonneaupl@gmail.com>
2017-02-14 14:26:26 -0500

    c

--- main.py
+++ main.py
@@ -4,11 +4,6 @@ from subprocess import check_output
 
 bigipaddr = "10.1.1.4"
 
-#for f in glob.glob('./*.json.deploy'):
-#    print f
-#    os.system("./scripts/deploy_iapp_bigip.py -r " + bigipaddr + " " + f)
-
-cmd = "git log --name-status -1"
 p = check_output(["git", "log", "--name-status", "-1"])
 
 for line in p.splitlines():
