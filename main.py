5880cebc814041d0f999c644bc67a7abf3308b6b
plcharbonneau <charbonneaupl@gmail.com>
2017-02-14 13:09:32 -0500

    commit

--- main.py
+++ main.py
@@ -15,4 +15,5 @@ for line in p.splitlines():
     parse = line.split( )
     if parse:
         if parse.pop(0) is "M":
-            print(line)
+            print(parse.pop(1))
+            #os.system("./scripts/deploy_iapp_bigip.py -r " + bigipaddr + " " + parse.pop())
