c8637432601717e6bbb2d76d55325544f6c9a790
plcharbonneau <charbonneaupl@gmail.com>
2017-02-14 14:29:25 -0500

    c

--- main.py
+++ main.py
@@ -6,15 +6,14 @@ bigipaddr = "10.1.1.4"
 
 p = check_output(["git", "log", "--name-status", "-1"])
 
+print("List of services to Add/Modify/Remove")
 for line in p.splitlines():
     parse = line.split("\t")
-    print("List of services to Add/Modify/Remove")
     if len(parse) >= 2:
         print(parse)
 
 for line in p.splitlines():
     if len(parse) >= 2:
-        print(parse)
         if parse[1].endswith(".json.deploy"):
             if parse[0] == "M":
                 os.system("./scripts/deploy_iapp_bigip.py -r " + bigipaddr + " " + parse[1])
