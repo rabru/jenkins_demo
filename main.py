fb6956c8a91e421585c03086eb9a27c387b85c39
plcharbonneau <charbonneaupl@gmail.com>
2017-02-14 12:44:41 -0500

    commit

--- main.py
+++ main.py
@@ -9,7 +9,7 @@ bigipaddr = "10.1.1.4"
 #    os.system("./scripts/deploy_iapp_bigip.py -r " + bigipaddr + " " + f)
 
 cmd = "git log --name-status -1"
-p = check_output(cmd)
+p = check_output(["git", "log", "--name-status", "-1"])
 
 for line in p.stdout:
     print l
