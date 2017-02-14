28ac52de2ff9e3356cb5d480923803baff2aa082
plcharbonneau <charbonneaupl@gmail.com>
2017-02-14 13:07:18 -0500

    commit

--- main.py
+++ main.py
@@ -14,6 +14,5 @@ p = check_output(["git", "log", "--name-status", "-1"])
 for line in p.splitlines():
     parse = line.split( )
     if parse:
-        print(parse.pop(0))
-#    if parse.index[self,0] is "m":
-#        print(line)
+        if parse.index[self,0] is "M":
+            print(line)
