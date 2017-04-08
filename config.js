7be12d4a71445b4549a9a078a8315e96f639137e
Pier-Luc Charbonneau <charbonneaupl@gmail.com>
2017-04-08 13:06:31 +0000

    Add deploy library

--- config.js
+++ config.js
@@ -1,7 +1,9 @@
 var config = {};
 
+config.strictSSL = false;
 config.iWorkflow = {};
 
+config.iWorkflow.ip = '10.1.1.6';
 config.iWorkflow.username = 'admin';
 config.iWorkflow.password = 'admin';
 
