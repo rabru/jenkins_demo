929a9e7ee4cc8ae216cb1ab4df172e4299152358
Pier-Luc Charbonneau <charbonneaupl@gmail.com>
2017-04-08 13:50:20 +0000

    ...

--- config.js
+++ config.js
@@ -4,7 +4,7 @@ config.strictSSL = false;
 config.iWorkflow = {};
 
 config.iWorkflow.ip = '10.1.1.6';
-config.iWorkflow.username = 'admin';
-config.iWorkflow.password = '@gility';
+config.iWorkflow.username = 'tenant';
+config.iWorkflow.password = 'tenant';
 
 module.exports = config;
--- redeployAllServices.js
+++ redeployAllServices.js
@@ -1,3 +1,4 @@
 var config = require('./config');
+var deployService = require('./lib/deploy_service_iWF');
 
-
+deployService.deployServiceiWF(config.iWorkflow.ip,config.iWorkflow.username,config.iWorkflow.password, config.strictSSL);
