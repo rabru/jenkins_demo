321c1482392c306a630438b7a57b14e4a2b93015
Pier-Luc Charbonneau <charbonneaupl@gmail.com>
2017-04-08 10:53:28 -0400

    commit

--- redeployAllServices.js
+++ redeployAllServices.js
@@ -1,4 +1,8 @@
 var config = require('./config');
 var deployService = require('./lib/deploy_service_iWF');
+var hashFile = require('./lib/hashFile').hashFile;
 
-deployService.deployServiceiWF(config.iWorkflow.ip,config.iWorkflow.username,config.iWorkflow.password, config.strictSSL);
+//deployService.deployServiceiWF(config.iWorkflow.ip,config.iWorkflow.username,config.iWorkflow.password, config.strictSSL);
+hashFile('main.py', function(data) {
+	console.log(data);
+});
